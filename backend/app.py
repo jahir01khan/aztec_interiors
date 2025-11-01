66d78db8a3e5ec0d1987ab117ea3c54becb8c215
Raza Taiab <razataiab25.11@gmail.com>
2025-11-01 18:41:35 +0000

    fixed port

--- backend/app.py
+++ backend/app.py
@@ -64,4 +64,4 @@ if __name__ == '__main__':
         db.create_all()
         print("Database tables created successfully!")
     
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
