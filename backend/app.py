b94bb46682e6b76ad40373783c69765b43f2b2db
N1144857 <ayaan.shaikh2022@my.ntu.ac.uk>
2025-11-02 21:22:08 +0000

    added gunicorn

--- backend/app.py
+++ backend/app.py
@@ -131,4 +131,4 @@ if __name__ == '__main__':
         print("✅ Server starting...")
         print("=" * 50)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
